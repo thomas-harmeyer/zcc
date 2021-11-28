fc72bc7a0f1c07fee32b4ddd342afe14f7ed4a21
Thomas Harmeyer <thomasmharmeyer@gmail.com>
2021-11-28 11:11:00 -0500

    updated server for more general cases

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ fs = require('fs');
 
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -14,24 +14,25 @@ app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
 
-const getAllTickets = (username, passwrod, subdomain) => {
+const getAllTickets = (username, password, subdomain) => {
 
     const config = {
         method: 'get',
-        url: 'https://zccthomasharmeyer.zendesk.com/api/v2/requests',
-        headers: {
-           
+        url: 'https://' + subdomain + '.zendesk.com/api/v2/requests',
+        auth: {
+            username: username,
+            password: password
         }
     };
 
-    axios(config).then(function (response) {
+    axios(config).then((response) => {
         const data = JSON.stringify(response.data);
         console.log(data);
         fs.writeFile("response.json", data, (error) => { if (error) console.log(error) });
         return data;
 
-    }).catch(function (error) {
-        console.log(error);
+    }).catch((error) => {
+        console.log("error\n" + error);
         return undefined;
     });
 }
