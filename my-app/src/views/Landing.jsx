d524b7b5ea7cbfbfc64667298042f2be5b18ef19
Thomas Harmeyer <thomasmharmeyer@gmail.com>
2021-11-28 18:40:01 -0500

    working basic and api auth

--- my-app/src/views/Landing.jsx
+++ my-app/src/views/Landing.jsx
@@ -2,9 +2,12 @@ import axios from "axios";
 import { useState } from "react";
 import FormControl from "react-bootstrap//FormControl";
 import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
 import Col from "react-bootstrap/Col";
+import { alignPropType } from "react-bootstrap/esm/types";
 import InputGroup from "react-bootstrap/InputGroup";
 import Row from "react-bootstrap/Row";
+import ToggleButton from "react-bootstrap/ToggleButton";
 import { Navigate, useLocation } from "react-router-dom";
 import Cookies from "universal-cookie";
 import Links from "../components/Links";
@@ -14,31 +17,38 @@ const Landing = () => {
   //cookies
   const cookies = new Cookies();
   const [redirectToTicketGallery, setRedirectToTicketGallery] = useState(false);
-  const [username, setUsername] = useState(
-    cookies.get("username") ? cookies.get("username") : ""
-  );
-  const [password, setPassword] = useState(
-    cookies.get("password") ? cookies.get("password") : ""
-  );
-  const [subdomain, setSubdomain] = useState(
-    cookies.get("subdomain") ? cookies.get("subdomain") : ""
-  );
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [subdomain, setSubdomain] = useState("");
+
+  //radios
+  const [radioValue, setRadioValue] = useState("basicAuth");
 
   //url
   const search = useLocation().search;
   const params = new URLSearchParams(search);
   const isError = params.get("error");
 
+  //radios
+  const radios = [
+    { name: "Basic Auth", value: "basicAuth" },
+    { name: "API Token", value: "apiToken" },
+  ];
+
   //put in stuff to store input vals
   const submit = async () => {
+    const type = radioValue;
     cookies.set("username", username);
     cookies.set("password", password);
     cookies.set("subdomain", subdomain);
 
     var data = JSON.stringify({
-      username: username,
-      password: password,
-      subdomain: subdomain,
+      type: type,
+      data: {
+        username: username,
+        password: password,
+        subdomain: subdomain,
+      },
     });
 
     var config = {
@@ -60,6 +70,51 @@ const Landing = () => {
         console.log(error);
       });
   };
+  const renderBasicAuth = () => {
+    return (
+      <>
+        <Row className="mb-4 justify-content-center">
+          <Col sm="auto">
+            <InputGroup>
+              <InputGroup.Text className="bg-secondary">Email</InputGroup.Text>
+              <FormControl
+                type="email"
+                placeholder="example@gmail.com"
+                value={username}
+                onInput={(e) => setUsername(e.target.value)}
+              />
+            </InputGroup>
+          </Col>
+          <Col sm={{ span: 4 }}>
+            <InputGroup>
+              <InputGroup.Text className="bg-secondary">
+                {radioValue === "basicAuth" ? "Password" : "API Token"}
+              </InputGroup.Text>
+              <FormControl
+                type="password"
+                value={password}
+                onInput={(e) => setPassword(e.target.value)}
+              />
+            </InputGroup>
+          </Col>
+        </Row>
+        <Row className="mb-4 justify-content-center">
+          <Col sm="auto">
+            <InputGroup>
+              <InputGroup.Text className="bg-secondary">
+                Subdomain
+              </InputGroup.Text>
+              <FormControl
+                value={subdomain}
+                onInput={(e) => setSubdomain(e.target.value)}
+              />
+              <InputGroup.Text>.zendesk.com</InputGroup.Text>
+            </InputGroup>
+          </Col>
+        </Row>
+      </>
+    );
+  };
 
   return (
     <div className="center-content-vertically">
@@ -73,43 +128,27 @@ const Landing = () => {
       <Row className="text-center">
         <h1>Zendesk Coding Challenge</h1>
       </Row>
-      <Row className="mb-4">
-        <Col sm={{ span: 4, offset: 2 }}>
-          <InputGroup>
-            <InputGroup.Text className="bg-secondary">Email</InputGroup.Text>
-            <FormControl
-              type="email"
-              placeholder="example@gmail.com"
-              value={username}
-              onInput={(e) => setUsername(e.target.value)}
-            />
-          </InputGroup>
-        </Col>
-        <Col sm={{ span: 4 }}>
-          <InputGroup>
-            <InputGroup.Text className="bg-secondary">Password</InputGroup.Text>
-            <FormControl
-              type="password"
-              value={password}
-              onInput={(e) => setPassword(e.target.value)}
-            />
-          </InputGroup>
+      <Row className="p-3 justify-content-md-center">
+        <Col sm="auto">
+          <ButtonGroup>
+            {radios.map((radio, index) => (
+              <ToggleButton
+                key={index}
+                id={`radio-${index}`}
+                type="radio"
+                variant={"outline-primary"}
+                name="radio"
+                value={radio.value}
+                checked={radioValue === radio.value}
+                onChange={(e) => setRadioValue(e.currentTarget.value)}
+              >
+                {radio.name}
+              </ToggleButton>
+            ))}
+          </ButtonGroup>
         </Col>
       </Row>
-      <Row className="mb-4">
-        <Col sm={{ span: "auto", offset: 4 }}>
-          <InputGroup>
-            <InputGroup.Text className="bg-secondary">
-              Subdomain
-            </InputGroup.Text>
-            <FormControl
-              value={subdomain}
-              onInput={(e) => setSubdomain(e.target.value)}
-            />
-            <InputGroup.Text>.zendesk.com</InputGroup.Text>
-          </InputGroup>
-        </Col>
-      </Row>
+      {renderBasicAuth()}
       <Row className="justify-content-md-center">
         <Col sm="auto">
           <Button onClick={submit}>Submit</Button>
@@ -119,5 +158,4 @@ const Landing = () => {
     </div>
   );
 };
-
 export default Landing;
--- server/index.js
+++ server/index.js
@@ -1,55 +1,72 @@
-const axios = require('axios');
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors  = require('cors');
+const axios = require("axios");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { config } = require("process");
 
-fs = require('fs');
+fs = require("fs");
 
 const app = express();
 const port = 3001;
 
 //cors options
 const options = {
-origin: 'http://localhost:3000',
-}
-    
+  origin: "http://localhost:3000",
+};
+
 app.use(cors(options));
 
 const jsonParser = bodyParser.json();
 
-app.post('/', jsonParser, (req, res) => {
-    res.set('Access-Control-Allow-Origin', '*'); 
-    const body = req.body;
-    console.log(req.body);
-    const data = body.username + " " + body.password + " " + body.subdomain;
-    const response = getAllTickets(body.username, body.password, body.subdomain);
-    response.then((data) => {
-        res.send(data);
-    });
+app.post("/", jsonParser, (req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  const body = req.body;
+  console.log(req.body);
+  const response = getAllTickets(body.type, body.data);
+  response.then((data) => {
+    res.send(data);
+  });
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
 
-const getAllTickets = (username, password, subdomain) => {
-    const config = {
-        method: 'get',
-        url: 'https://' + subdomain + '.zendesk.com/api/v2/requests',
-        auth: {
-            username: username,
-            password: password
-        }
+const getAllTickets = (type, data) => {
+  var config = {
+    method: "get",
+    url: "https://" + data.subdomain + ".zendesk.com/api/v2/requests",
+    headers: {},
+  };
+
+  const username = data.username;
+
+  if (type === "basicAuth") {
+    const password = data.password;
+    config.headers = {
+      Authorization:
+        "Basic " + Buffer.from(username + ":" + password).toString("base64"),
     };
+  } else if (type === "apiToken") {
+    const apiToken = data.password;
+    config.headers = {
+      Authorization:
+        "Basic " +
+        Buffer.from(username + "/token:" + apiToken).toString("base64"),
+    };
+  }
 
-   return axios(config).then((response) => {
-        const data = JSON.stringify(response.data);
-        console.log(data);
-        //fs.writeFile("response.json", data, (error) => { if (error) console.log(error) });
-        return data;
+  console.log(config);
 
-    }).catch((error) => {
-        console.log("error\n" + error);
-        return undefined;
+  return axios(config)
+    .then((response) => {
+      const data = JSON.stringify(response.data);
+      console.log(data);
+      //fs.writeFile("response.json", data, (error) => { if (error) console.log(error) });
+      return data;
+    })
+    .catch((error) => {
+      console.log("error\n" + error);
+      return undefined;
     });
-}
+};
